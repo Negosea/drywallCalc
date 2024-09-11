69a6d28758f6bfdce113b48e92ac032b84517fe6
nego_sea <acheiqueprocurava@gmail.com>
2024-09-10 22:49:12 -0300

    Atualiza setup.py e inclui MANIFEST.in

--- drywallCalc/setup.py
+++ drywallCalc/setup.py
@@ -3,13 +3,21 @@ from setuptools import setup, find_packages
 setup(
     name='drywallCalc',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages(),  # Encontra todos os pacotes e subpacotes
     install_requires=[
         # Liste as dependências aqui, se houver
     ],
     entry_points={
         'console_scripts': [
-            'drywallcalc=main:main',  # Altere para o ponto de entrada correto
+            'drywallcalc=drywallCalc.main:main',  # Altere para o ponto de entrada correto
         ],
     },
+    include_package_data=True,
+    description='Calculadora para cálculos de drywall',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Marcos Sea',
+    author_email='tecnosteelframe@gmail.com',
+    url='https://github.com/Negosea/drywallCalc',
+    license='MIT',  # Ou a licença que você escolher
 )
